07802bd04d896db803ed4aa0af0d70af5cb840ca
timdoan22 <timdoan01@gmail.com>
2022-04-28 22:41:39 -0400

    Adding solutions for April 28, 2022

---
+++ april_28_2022.js
@@ -0,0 +1,20 @@
+// Usually when you buy something. you’re asked whether your credit card number, phone number or answer to your most secret question is still correct. 
+// However, since someone could look over your shoulder, you don’t want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into ‘#’.
+
+function maskify(cc) {
+    let newStr = ''
+    for (i = 0; i < cc.length; i++) {
+        if (cc.length < 4) {
+            newStr = cc;
+        } else if (i <= cc.length - 5) {
+            newStr += '#';
+        } else {
+            newStr += cc[i]
+        }
+    }
+    return newStr
+}
+
+console.log(maskify('4556364607935616'))
