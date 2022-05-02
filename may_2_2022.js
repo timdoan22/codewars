a0a4b733bdbba44c8fdfe0ef3084637625d07b1b
timdoan22 <timdoan01@gmail.com>
2022-05-02 15:13:36 -0400

    Adding daily challenge for May 2, 2022

---
+++ may_2_2022.js
@@ -0,0 +1,13 @@
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+function openOrSenior(data) {
+	let returnList = []
+	for (num of data)
+      if(num[0] > 54 && num[1] > 7)
+      	returnList.push('Senior')
+      else
+        returnList.push('Open')
+  return returnList
+}
