5fde1890d5e5ffca29418752a5f36e0c1cc04728
timdoan22 <timdoan01@gmail.com>
2022-05-04 16:45:53 -0400

    Adding challenge for May 4, 2022

---
+++ may_4_2022.js
@@ -0,0 +1,32 @@
+// Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
+// Valid inputs examples:
+
+// Examples of valid inputs:
+// 1.2.3.4
+// 123.45.67.89
+
+// Invalid input examples:
+
+// 1.2.3
+// 1.2.3.4.5
+// 123.456.78.90
+// 123.045.067.089
+
+
+
+function isValidIP(str) {
+    const splitIP = str.split('.')
+    const octets = str.split("").filter(octet => octet === '.')
+    if (octets.length === 3) {
+  	    for (number of splitIP) {
+            if ((number[0] === '0' && Number(number) != 0) || number.includes(" ") === true || number.includes('\n') === true || number[1] === 'e' || Number(number) < 0 || Number(number) > 255 || isNaN(parseFloat(number)) === true) {
+                return false
+            }
+        }
+    }
+    else {
+        return false
+    }
+    return true
+}
+
