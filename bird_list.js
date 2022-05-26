cf373249c913f809b91693ca14d120ed3ff87058
timdoan22 <timdoan01@gmail.com>
2022-05-26 05:57:52 -0400

    Adding kata for May 26, 2022

---
+++ bird_list.js
@@ -0,0 +1,32 @@
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+//   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+
+// For example, if this array were passed as an argument:
+
+//  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
+
+// Your function would return the following array:
+
+// ["Mallard", "Hook Bill", "Crested", "Blue Swedish"]
+
+
+/* function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  let list = []
+  // return an array containing all of the strings in the input array except those that match strings in geese
+  for (word of birds) {
+    if (!(geese.includes(word)))
+      list.push(word)
+  }
+  return list
+}; */
+
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    // return an array containing all of the strings in the input array except those that match strings in geese
+    return birds.filter(word => (!(geese.includes(word))))
+  };
