f5a9a6124e02ba22275fb193f208e4789e9fec50
timdoan22 <timdoan01@gmail.com>
2022-08-22 00:00:00 +0900

    Adding kata for August 22, 2022

---
+++ number_to_string.js
@@ -0,0 +1,12 @@
+// We need a function that can transform a number (integer) into a string.
+
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+function numberToString(num) {
+    return num.toString()
+  }
+
